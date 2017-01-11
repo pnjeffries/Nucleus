8ed5662dd00ef6c38a85aa1fe046c3651cb01d3b
Paul Jeffries <jeffercake@hotmail.com>
2017-01-11 01:05:01 +0000

    Revit basic helper functions and extension methods added

---
+++ FreeBuild/FreeBuild.Revit/FamilyInstanceExtensions.cs
@@ -0,0 +1,123 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeBuild.Revit
+{
+    /// <summary>
+    /// Extension methods for Revit FamilyInstance objects
+    /// </summary>
+    public static class FamilyInstanceExtensions
+    {
+        /// <summary>
+        /// Set the value of a parameter on this instance
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameter"></param>
+        /// <param name="value"></param>
+        public static bool SetParameter(this FamilyInstance obj, BuiltInParameter parameter, int value)
+        {
+            Parameter param = obj.get_Parameter(parameter);
+            if (param != null) return param.Set(value);
+            else return false;
+        }
+
+        /// <summary>
+        /// Set the value of a parameter on this instance
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameter"></param>
+        /// <param name="value"></param>
+        public static bool SetParameter(this FamilyInstance obj, BuiltInParameter parameter, double value)
+        {
+            Parameter param = obj.get_Parameter(parameter);
+            if (param != null) return param.Set(value);
+            else return false;
+        }
+
+        /// <summary>
+        /// Set the value of a parameter on this instance
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameter"></param>
+        /// <param name="value"></param>
+        public static bool SetParameter(this FamilyInstance obj, BuiltInParameter parameter, string value)
+        {
+            Parameter param = obj.get_Parameter(parameter);
+            if (param != null) return param.Set(value);
+            else return false;
+        }
+
+        /// <summary>
+        /// Set the value of a parameter on this instance
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameter"></param>
+        /// <param name="value"></param>
+        public static bool SetParameter(this FamilyInstance obj, BuiltInParameter parameter, ElementId value)
+        {
+            Parameter param = obj.get_Parameter(parameter);
+            if (param != null) return param.Set(value);
+            else return false;
+        }
+
+        /// <summary>
+        /// Set the value of a parameter on this instance by name
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool SetParameter(this FamilyInstance obj, string parameterName, int value)
+        {
+            Parameter param = obj.LookupParameter(parameterName);
+            if (param != null) return param.Set(value);
+            else return false;
+        }
+
+        /// <summary>
+        /// Set the value of a parameter on this instance by name
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool SetParameter(this FamilyInstance obj, string parameterName, double value)
+        {
+            Parameter param = obj.LookupParameter(parameterName);
+            if (param != null) return param.Set(value);
+            else return false;
+        }
+
+        /// <summary>
+        /// Set the value of a parameter on this instance by name
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool SetParameter(this FamilyInstance obj, string parameterName, string value)
+        {
+            Parameter param = obj.LookupParameter(parameterName);
+            if (param != null) return param.Set(value);
+            else return false;
+        }
+
+        /// <summary>
+        /// Set the value of a parameter on this instance by name
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool SetParameter(this FamilyInstance obj, string parameterName, ElementId value)
+        {
+            Parameter param = obj.LookupParameter(parameterName);
+            if (param != null) return param.Set(value);
+            else return false;
+        }
+    }
+}
---
+++ FreeBuild/FreeBuild.Revit/RevitOutput.cs
@@ -0,0 +1,87 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AD = Autodesk.Revit.DB;
+using FB = FreeBuild.Geometry;
+
+namespace FreeBuild.Revit
+{
+    /// <summary>
+    /// Static output Revit helper functions
+    /// </summary>
+    public static class RevitOutput
+    {
+        public static Document Document
+        {
+            get
+            {
+                throw new NotFiniteNumberException();
+                //TODO: return Host.Instance.RevitDocument;
+            }
+        }
+
+        /// <summary>
+        /// Create a new structural member in the specified Revit document
+        /// </summary>
+        /// <param name="curve">The set-out curve of the member</param>
+        /// <param name="symbol">The family symbol of the member to create</param>
+        /// <param name="document">The document to add the member to</param>
+        /// <param name="distanceUnit">The distance unit the passed-in curve is expressed in</param>
+        /// <returns></returns>
+        public static FamilyInstance CreateStructuralFraming(FB.Curve curve, FamilySymbol symbol, StructuralType structuralType)
+        {
+            FamilyInstance instance = CreateFamilyInstance(symbol, structuralType);
+
+            AD.Curve rCrv = FBtoRevit.Convert(curve);
+            if (rCrv != null)
+            {
+                LocationCurve location = instance.Location as LocationCurve;
+                location.Curve = rCrv;
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Create a new column member in the specified Revit document
+        /// </summary>
+        /// <param name="curve"></param>
+        /// <param name="symbol"></param>
+        /// <param name="document"></param>
+        /// <param name="distanceUnit"></param>
+        /// <returns></returns>
+        public static FamilyInstance CreateColumn(FB.Curve curve, FamilySymbol symbol)
+        {
+            FamilyInstance instance = CreateStructuralFraming(curve, symbol, StructuralType.Column);
+            instance.SetParameter(BuiltInParameter.SLANTED_COLUMN_TYPE_PARAM, (int)SlantedOrVerticalColumnType.CT_EndPoint);
+            return instance;
+        }
+
+        /// <summary>
+        /// Create an instance of the specified family, located at the origin
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <param name="structuralType"></param>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        public static FamilyInstance CreateFamilyInstance(FamilySymbol symbol, StructuralType structuralType)
+        {
+            if (!symbol.IsActive) symbol.Activate();
+            return Document.Create.NewFamilyInstance(new XYZ(0, 0, 0), symbol, structuralType);
+        }
+
+        /// <summary>
+        /// Get a Revit FamilyInstance by it's element ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static FamilyInstance GetInstance(ElementId id)
+        {
+            FamilyInstance result = Document.GetElement(id) as FamilyInstance;
+            return result;
+        }
+    }
+}
