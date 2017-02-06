dd8ebc056218425a52b0cf2deed26df3535c0e13
Paul Jeffries <jeffercake@hotmail.com>
2017-02-06 23:29:23 +0000

    Core OpenGL 3D rendering routines begun

---
+++ FreeBuild/FreeBuild.OpenTK/RenderControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+
+namespace FreeBuild.OpenTK
+{
+    public partial class RenderControl : GLControl
+    {
+        public RenderControl() : base() // Could set up specialised GraphicsMode here
+        {
+            InitializeComponent();
+        }
+    }
+}
