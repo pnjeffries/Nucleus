f375e79f889310ca2b5d4aa592e6704a5ff80577
Paul Jeffries <jeffercake@hotmail.com>
2017-08-14 21:21:30 +0100

    Display unit scaling converters added

---
+++ Nucleus/Nucleus.WPF/Converters/MillimetersConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Nucleus.WPF.Converters
+{
+    /// <summary>
+    /// Value converter to convert between distances stored in m but represented in mm
+    /// </summary>
+    public class MillimetersConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert from m to mm
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double v = (double)value;
+            return v * 1000;
+        }
+
+        /// <summary>
+        /// Convert from mm to m
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double v = (double)value;
+            return v / 1000;
+        }
+    }
+}
