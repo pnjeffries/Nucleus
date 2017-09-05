58c19948f5d757c3df00378477c6726caee26ad0
Paul Jeffries <jeffercake@hotmail.com>
2017-09-05 18:07:14 +0100

    Minor tweaks to WPF controls

---
+++ Nucleus/Nucleus.WPF/Converters/SliderConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Nucleus.WPF.Converters
+{
+    public class SliderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return 0;
+            else if (value is string)
+            {
+                return double.Parse((string)value);
+            }
+            else return value;
+
+        }
+    }
+}
--- Nucleus/Nucleus.WPF/MultiSelectListBox.cs
+++ Nucleus/Nucleus.WPF/MultiSelectListBox.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,26 @@ namespace Nucleus.WPF
     /// </summary>
     public class MultiSelectListBox : ListBox
     {
+        #region Fields
+
+        /// <summary>
+        /// Flag that is used to prevent infinite back-and-forth between SelectedItems
+        /// and the bound Selection's modification events
+        /// </summary>
+        private bool _LockSelectionUpdates = false;
+
+        #endregion
+
         #region Properties
 
+        private static void OnSelectionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((MultiSelectListBox)d).RegisterNewSelectionEvents(e.NewValue, e.OldValue);
+        }
+
         public static readonly DependencyProperty SelectionProperty =
-            DependencyProperty.Register("Selection", typeof(IList), typeof(MultiSelectListBox));
+            DependencyProperty.Register("Selection", typeof(IList), typeof(MultiSelectListBox),
+               new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.None, new PropertyChangedCallback(OnSelectionChanged)));
 
         /// <summary>
         /// The currently selected items.
@@ -39,14 +56,55 @@ namespace Nucleus.WPF
             SelectionMode = SelectionMode.Multiple;
         }
 
+        private void RegisterNewSelectionEvents(object newValue, object oldValue)
+        {
+            if (oldValue is INotifyCollectionChanged)
+            {
+                var oldNCC = (INotifyCollectionChanged)oldValue;
+                oldNCC.CollectionChanged -= Selection_CollectionChanged;
+            }
+
+            if (newValue is INotifyCollectionChanged)
+            {
+                var newNCC = (INotifyCollectionChanged)newValue;
+                newNCC.CollectionChanged += Selection_CollectionChanged;
+            }
+        }
+
+        private void Selection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (!_LockSelectionUpdates)
+            {
+                _LockSelectionUpdates = true;
+                if (e.Action == NotifyCollectionChangedAction.Reset) SelectedItems.Clear();
+                if (e.OldItems != null)
+                {
+                    if (e.OldItems.Count == SelectedItems.Count) SelectedItems.Clear();
+                    else foreach (var item in e.OldItems) SelectedItems.Remove(item);
+                }
+                if (e.NewItems != null)
+                {
+                    foreach (var item in e.NewItems)
+                        if (!SelectedItems.Contains(item))
+                            SelectedItems.Add(item);
+                }
+                _LockSelectionUpdates = false;
+            }
+        }
+
         private void MultiSelectListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (e.RemovedItems.Count == Selection.Count) Selection.Clear();
-            else foreach (var item in e.RemovedItems) Selection.Remove(item);
+            if (!_LockSelectionUpdates)
+            {
+                _LockSelectionUpdates = true;
+                if (e.RemovedItems.Count == Selection.Count) Selection.Clear();
+                else foreach (var item in e.RemovedItems) Selection.Remove(item);
 
-            foreach (var item in e.AddedItems)
-                if (!Selection.Contains(item))
-                    Selection.Add(item);
+                foreach (var item in e.AddedItems)
+                    if (!Selection.Contains(item))
+                        Selection.Add(item);
+                _LockSelectionUpdates = false;
+            }
         }
 
         #endregion
