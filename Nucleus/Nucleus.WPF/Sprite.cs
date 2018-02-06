75fdcb81fe4cba856d2919bceaff722bf2b2f72f
pnjeffries <jeffercake@hotmail.com>
2018-02-06 00:41:12 +0000

    WPF sprite class created

--- Nucleus/Nucleus.WPF/Sprite.cs
+++ Nucleus/Nucleus.WPF/Sprite.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 using Nucleus.Rendering;
+using System.Windows.Media;
+using System.Windows;
 
 namespace Nucleus.WPF
 {
     /// <summary>
     /// A WPF control which represents an animated 2D Sprite
     /// </summary>
-    public class Sprite : Image
+    public class Sprite : Shape
     {
+        #region Fields
+
+        private Rect _Rectangle = Rect.Empty;
+
+        #endregion
+
         #region Properties
 
         /// <summary>
@@ -29,6 +38,111 @@ namespace Nucleus.WPF
             set { _SpriteData = value; }
         }
 
+        public override System.Windows.Media.Geometry RenderedGeometry
+        {
+            get { return new RectangleGeometry(_Rectangle); }
+        }
+
+        protected override System.Windows.Media.Geometry DefiningGeometry
+        {
+            get { return new RectangleGeometry(_Rectangle); }
+        }
+
+        public override Transform GeometryTransform => Transform.Identity;
+
+        #endregion
+
+        #region Constructors
+
+        public Sprite() { }
+
+        static Sprite()
+        {
+            StretchProperty.OverrideMetadata(typeof(Sprite), new FrameworkPropertyMetadata(Stretch.Fill));
+        }
+
+        #endregion
+
+        #region Method
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            drawingContext.DrawRectangle(Fill, null, _Rectangle);
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            if (Stretch == Stretch.UniformToFill)
+            {
+                double width = constraint.Width;
+                double height = constraint.Height;
+
+                if (Double.IsInfinity(width) && Double.IsInfinity(height))
+                {
+                    return new Size();
+                }
+                else if (Double.IsInfinity(width) || Double.IsInfinity(height))
+                {
+                    width = Math.Min(width, height);
+                }
+                else
+                {
+                    width = Math.Max(width, height);
+                }
+
+                return new Size(width, width);
+            }
+
+            return new Size();
+        }
+
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+
+
+            _Rectangle = new Rect(0, 0, 
+                Math.Max(0, finalSize.Width), Math.Max(0, finalSize.Height));
+
+            switch (Stretch)
+            {
+                case Stretch.None:
+                    _Rectangle.Width = _Rectangle.Height = 0;
+                    break;
+
+                case Stretch.Fill:
+                    break;
+
+                case Stretch.Uniform:
+                    if (_Rectangle.Width > _Rectangle.Height)
+                    {
+                        _Rectangle.Width = _Rectangle.Height;
+                    }
+                    else
+                    {
+                        _Rectangle.Height = _Rectangle.Width;
+                    }
+                    break;
+
+                case Stretch.UniformToFill:
+
+                    // The minimal square that fills the final box
+                    if (_Rectangle.Width < _Rectangle.Height)
+                    {
+                        _Rectangle.Width = _Rectangle.Height;
+                    }
+                    else  // _rect.Width >= _rect.Height
+                    {
+                        _Rectangle.Height = _Rectangle.Width;
+                    }
+                    break;
+            }
+
+
+            //ResetRenderedGeometry();
+
+            return finalSize;
+        }
 
         #endregion
     }
